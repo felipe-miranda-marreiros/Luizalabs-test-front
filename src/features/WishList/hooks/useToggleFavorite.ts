962ac22670a1013874236308c880b0ce53b28427
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2025-06-25 13:52:23 -0300

    refactor: move favorite toggle to useToggleFavorite

--- src/features/WishList/hooks/useToggleFavorite.ts
+++ src/features/WishList/hooks/useToggleFavorite.ts
@@ -1,9 +1,26 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { wishListService } from '../services/wishListService'
+import { useState } from 'react'
 
-export function useToggleFavorite() {
+export function useToggleFavorite(id: string) {
+  const [isFavorite, setIsFavorite] = useState(() =>
+    wishListService.isOnWishList(parseInt(id))
+  )
+  const queryClient = useQueryClient()
+
   const mutation = useMutation({
-    mutationFn: (id: number) => wishListService.add(id)
+    mutationFn: () => wishListService.add(parseInt(id)),
+    onSuccess: async () => {
+      setIsFavorite((prevState) => !prevState)
+      await queryClient.invalidateQueries({ queryKey: ['FAVORITE_COUNT'] })
+    },
+    onError: () => {
+      setIsFavorite(false)
+    }
   })
-  return mutation
+
+  return {
+    isFavorite,
+    onToggle: mutation.mutate
+  }
 }
--- src/features/WishList/ui/Favorite.tsx
+++ src/features/WishList/ui/Favorite.tsx
@@ -1,38 +1,23 @@
 import { cn } from '@/shared/lib'
 import { Heart } from 'lucide-react'
-import { useState } from 'react'
 import { useToggleFavorite } from '../hooks/useToggleFavorite'
-import { wishListService } from '../services/wishListService'
-import { useQueryClient } from '@tanstack/react-query'
 
 interface FavoriteProps {
   id: string
 }
 
 export function Favorite({ id }: FavoriteProps) {
-  const [isFavorite, setIsFavorite] = useState(() =>
-    wishListService.isOnWishList(parseInt(id))
-  )
-  const { mutate } = useToggleFavorite()
-  const queryClient = useQueryClient()
+  const { isFavorite, onToggle } = useToggleFavorite(id)
 
-  function onToggle() {
-    mutate(parseInt(id), {
-      onSuccess: async () => {
-        setIsFavorite((prevState) => !prevState)
-        await queryClient.invalidateQueries({ queryKey: ['FAVORITE_COUNT'] })
-      },
-      onError: (err) => {
-        console.log(err)
-      }
-    })
+  function onClick() {
+    onToggle()
   }
 
   return (
     <button
       title={isFavorite ? 'Remover dos favoritos' : 'Adicionar aos favoritos'}
       className="cursor-pointer"
-      onClick={onToggle}
+      onClick={onClick}
       type="button"
     >
       <Heart className={cn(isFavorite && 'fill-red-500')} />
