439d9dbf5244199a1d786343eda95b1b8af81e41
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2025-06-26 13:15:23 -0300

    chore: update api baseURL

--- src/entities/Product/services/productApi.ts
+++ src/entities/Product/services/productApi.ts
@@ -2,7 +2,7 @@ import { api } from '@/shared/api'
 import type { ProductAPI } from '../models/product'
 
 async function productList(): Promise<ProductAPI[]> {
-  const response = await api.get<ProductAPI[]>('/products')
+  const response = await api.get<ProductAPI[]>('api/products')
   return response.data
 }
 
