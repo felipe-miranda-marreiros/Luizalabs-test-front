fb975b1076a8ec58a2e1969332c6012573ee25b1
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2025-06-27 15:44:18 -0300

    test: ensure login page navigates to sign-up page

--- src/pages/Login/ui/LoginPage.test.tsx
+++ src/pages/Login/ui/LoginPage.test.tsx
@@ -1,10 +1,22 @@
-import { Routes } from '@/app/routes/routes'
-import { render } from '@/shared/test/test-utils'
-import { screen } from '@testing-library/dom'
 import userEvent from '@testing-library/user-event'
+import { render } from '@/shared/test/test-utils'
+import { screen, waitFor } from '@testing-library/dom'
+import { Routes } from '@/app/routes/routes'
 import { MemoryRouter } from 'react-router'
+import { server } from '@/shared/test'
+
+jest.unmock('react-router')
 
 describe('<LoginPage />', () => {
+  beforeAll(() => server.listen())
+
+  afterEach(() => server.resetHandlers())
+
+  afterAll(() => {
+    server.close()
+    jest.resetAllMocks()
+  })
+
   test('Should navigate to sign-up page when clicking "Fazer cadastro"', async () => {
     render(
       <MemoryRouter initialEntries={['/']}>
@@ -18,5 +30,24 @@ describe('<LoginPage />', () => {
     expect(
       await screen.findByText(/Faça o cadastro da sua conta/i)
     ).toBeDefined()
+  })
+  test('Should navigate to products page on login success', async () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <Routes />
+      </MemoryRouter>
+    )
+
+    const emailInput = screen.getByPlaceholderText(/seu e-mail/i)
+    const passwordInput = screen.getByPlaceholderText(/sua senha/i)
+    const submitButton = screen.getByRole('button', { name: /entrar/i })
+
+    await userEvent.type(emailInput, 'user@email.com')
+    await userEvent.type(passwordInput, '123456')
+    await userEvent.click(submitButton)
+
+    await waitFor(() => {
+      expect(screen.getByTestId('product-page')).toBeInTheDocument()
+    })
   })
 })
