4604ee4f46c6d73049a8e52ae26e0ba9523b6f4a
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2025-06-27 15:51:12 -0300

    test: ensure providers can render correctly children

--- src/app/providers/providers.spec.tsx
+++ src/app/providers/providers.spec.tsx
@@ -1,21 +1,13 @@
 import { render, screen } from '@testing-library/react'
 import { Providers } from './providers'
-import type { PropsWithChildren } from 'react'
-
-jest.mock('../hooks/setup', () => ({
-  Setup: ({ children }: PropsWithChildren) => (
-    <div data-testid="setup">{children}</div>
-  )
-}))
 
 jest.mock('../routes/routes', () => ({
   Routes: () => <div data-testid="routes">Routes content</div>
 }))
 
 describe('<Providers />', () => {
-  it('Should render QueryClientProvider, BrowserRouter, Setup and Routes', () => {
+  it('Should render QueryClientProvider, BrowserRouter, and Routes', () => {
     render(<Providers />)
-    expect(screen.getByTestId('setup')).toBeInTheDocument()
     expect(screen.getByTestId('routes')).toBeInTheDocument()
     expect(screen.getByText('Routes content')).toBeInTheDocument()
   })
