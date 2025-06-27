7ae4c0e941c876fe2aded308ec0fe528190a7bc5
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2025-06-27 15:41:03 -0300

    feat: return queryClient from useToggleFavorite

--- src/entities/Wish/hooks/useToggleFavorite.ts
+++ src/entities/Wish/hooks/useToggleFavorite.ts
@@ -46,6 +46,7 @@ export function useToggleFavorite(id: string) {
   return {
     isFavorite,
     onToggle: mutation.mutate,
-    isPending: mutation.isPending
+    isPending: mutation.isPending,
+    queryClient: queryClient
   }
 }
